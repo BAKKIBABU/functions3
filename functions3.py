aeabb8a2bfd21121b73e566e765c5cfc22b5adcd
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-08-02 16:37:54 +0530

    Add files via upload

---
+++ functions3.py
@@ -0,0 +1,64 @@
+# filter() function that returs even numbers from a list
+#filter, map, reduce,lambda,zip  functions very very importent
+# def even(x):
+#     if x%2==0:
+#         return True
+#     else:
+#         return False
+# z=[int(x) for x in input('enter numbers').split()]
+# # q=list(filter(even,z))
+# q=list(filter(lambda x:x%2==0,z))
+# print(q)
+# # print("from the z the even value are:",q)
+# def even(x):
+#     if x%2==1:
+#         return True
+#     else:
+#         return False
+# z=[int(x) for x in input().split()]
+# q=list(filter(even,z))
+# print(q)
+# def even(x):
+#     if x%2!=0:
+#         return True
+#     else:
+#         return False
+# z=[int(x) for x in input().split()]
+# q=list(filter(even,z))
+# print(q)
+# def even(x):
+#     if x%3==0:
+#         return True
+#     else:
+#         return False
+# z=[int(x) for x in input().split()]
+# q=list(filter(even,z))
+# print(q)
+# lamda function to calculate sum of two numbers
+# f=lambda x,y,z: x+y+z
+# s=lambda x,y,z: x*y*z
+# r=f(3,10,2)
+# z=s(2,3,5)
+# print('sum=',r)
+# print('sum=',z)
+# a lambda functon that returns bigger number
+# m=lambda x,y: x if x<y else y
+# x,y=[int(n) for n in input("enter two numbers:").spilt()]
+# print(m(x,y))
+
+# a=[int(n) for n in input("enter two numbers:").split()]
+# print("Bigger number is:",max(a))
+# print("Smaller number is:",max(a))
+# a=20,39,50
+# print(max(a))
+# print(min(a))
+# a lambda function that returns even numbers from a list
+# a=[int(x) for x in input().spit()]
+# b=list(filter(lambdax: (x%2==0),a))
+# c=list(filter(lambdax: (x%2!=0),a))
+# print(b)
+# print(c)
+# a lambda function to calculate square value
+# f=lambda x: x**2
+# value=f(35)
+# print('square of 4=',value)
